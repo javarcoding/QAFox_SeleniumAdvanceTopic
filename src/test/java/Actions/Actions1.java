f3d93fa9411d269c1197d4de0ba45289a115dacb
adsul <adsul@Raviraj>
2023-02-12 12:00:16 +0530

    1

---
+++ src/test/java/Actions/Actions1.java
@@ -0,0 +1,22 @@
+package Actions;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Actions1 {
+
+	public static void main(String[] args) {
+		
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		driver.get("");
+		
+		
+	}
+}
+  
---
+++ src/test/java/Autoit/AutoIT.java
@@ -0,0 +1,15 @@
+package Autoit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class AutoIT {
+
+	public static void main(String[] args) {
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+	}
+}
